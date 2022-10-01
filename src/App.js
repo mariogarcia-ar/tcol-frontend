6f3632d1528727cdfc451549de78f358b3f8757b
Mario Garcia <mariogarcia.ar@gmail.com>
2022-10-01 04:45:13 +0000

    Movemos la carpeta de uploads

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () =>{
         <header className="App-header">
             <Navbar bg="dark" variant="dark">
               <Container>
-                <Navbar.Brand><Link to={"./create-credit"}  className="nav-link">React MERN stack App</Link></Navbar.Brand>
+                <Navbar.Brand><Link to={"./create-credit"}  className="nav-link">TCOL :: Proof Of Liquidity</Link></Navbar.Brand>
                 <Navbar className="justify-content-end">
                   <Nav><Link to={"./create-credit"}  className="nav-link">Create Credit</Link></Nav>
                   <Nav><Link to={"./credit-list"}  className="nav-link">Credit List</Link></Nav>
